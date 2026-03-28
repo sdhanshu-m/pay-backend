f1fa9888d9656e12ff1f47d7d2e4bdc903ca6c79
sudhanshu mishra <sudhanshum934@gmail.com>
2026-03-28 17:01:40 +0530

    debug jwt

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,16 +1,27 @@
 export const protect = (req, res, next) => {
+  console.log("---- REQUEST HIT ----");
+
+  console.log("AUTH HEADER:", req.headers.authorization);
+
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
+    console.log("TOKEN:", token);
+
     if (!token) {
+      console.log("NO TOKEN");
       return res.status(401).json({ message: "Unauthorized" });
     }
 
-    const decoded = jwt.verify(token, env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("DECODED:", decoded);
+
     req.user = decoded;
 
     next();
   } catch (err) {
+    console.log("JWT ERROR:", err.message);
     return res.status(401).json({ message: "Invalid token" });
   }
 };
