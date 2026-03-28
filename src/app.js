b281815c8d4174b840304feed5a8b71a97e8a8b6
sudhanshu mishra <sudhanshum934@gmail.com>
2026-03-28 18:07:48 +0530

    add frontend url

--- src/app.js
+++ src/app.js
@@ -7,13 +7,14 @@ import paymentRoutes from "./routes/payment.routes.js";
 const app = express();
 
 // ✅ Proper CORS (this alone is enough)
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://pay-frontend-beta.vercel.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 // ✅ JSON parser
 app.use(express.json());
